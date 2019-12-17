f423e1ba0578287c84a3f0fca56d350191fe50be
Małgorzata Świeca <mitorajmalgorzata@gmail.com>
2019-12-17 14:08:44 +0100

    Fix a broken link (#44)

--- katacoda-tutorials/setup.sh
+++ katacoda-tutorials/setup.sh
@@ -1,4 +1,4 @@
-echo "Upgrade to Helm 1.16.0"
+echo "Upgrade to Helm 2.16.0"
 curl -LO https://get.helm.sh/helm-"v2.16.0"-linux-amd64.tar.gz
 tar -xzvf helm-"v2.16.0"-linux-amd64.tar.gz
 mv ./linux-amd64/{helm,tiller} /usr/local/bin
